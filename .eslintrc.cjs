cedbd9ea134d66296ca18f3da257301a034a6b35
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2023-02-15 22:44:16 +0000

    chore: add eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,55 @@
+module.exports = {
+  root: true,
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [
+    {
+      files: ['*.tsx, *.jsx'],
+      rules: {
+        '@typescript-eslint/ban-types': [
+          'error',
+          {
+            extendDefaults: true,
+            types: {
+              '{}': false,
+            },
+          },
+        ],
+      },
+    },
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-unused-vars': 'off',
+    'no-undef': process.env.NODE_ENV === 'production' ? 'warn' : 'error',
+    'no-unreachable': process.env.NODE_ENV === 'production' ? 'warn' : 'error',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/no-unused-vars':
+      process.env.NODE_ENV === 'production' ? 'warn' : 'error',
+  },
+};
